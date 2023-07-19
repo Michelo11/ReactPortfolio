66fdbd61319c39bf80c49ef7f6d0c35d0e65a4ed
Lorenzo <hello@lorenzo0111.me>
2023-07-19 14:34:33 +0200

    Updated supabase types

--- types/supabase.ts
+++ types/supabase.ts
@@ -9,20 +9,48 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
+      admins: {
+        Row: {
+          user_id: string;
+        };
+        Insert: {
+          user_id: string;
+        };
+        Update: {
+          user_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "admins_user_id_fkey";
+            columns: ["user_id"];
+            referencedRelation: "users";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
       chat: {
         Row: {
+          commission: number;
           id: number;
           owner: string;
         };
         Insert: {
+          commission: number;
           id?: number;
           owner?: string;
         };
         Update: {
+          commission?: number;
           id?: number;
           owner?: string;
         };
         Relationships: [
+          {
+            foreignKeyName: "chat_commission_fkey";
+            columns: ["commission"];
+            referencedRelation: "commissions";
+            referencedColumns: ["id"];
+          },
           {
             foreignKeyName: "chat_owner_fkey";
             columns: ["owner"];
@@ -68,12 +96,67 @@ export interface Database {
           },
         ];
       };
+      commissions: {
+        Row: {
+          complete: boolean;
+          created_at: string;
+          id: number;
+          owner: string;
+          value: number;
+        };
+        Insert: {
+          complete?: boolean;
+          created_at?: string;
+          id?: number;
+          owner: string;
+          value?: number;
+        };
+        Update: {
+          complete?: boolean;
+          created_at?: string;
+          id?: number;
+          owner?: string;
+          value?: number;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "commissions_owner_fkey";
+            columns: ["owner"];
+            referencedRelation: "users";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      earnings: {
+        Row: {
+          amount: number;
+          commission: string | null;
+          date: string;
+          id: number;
+        };
+        Insert: {
+          amount: number;
+          commission?: string | null;
+          date?: string;
+          id?: number;
+        };
+        Update: {
+          amount?: number;
+          commission?: string | null;
+          date?: string;
+          id?: number;
+        };
+        Relationships: [];
+      };
     };
     Views: {
       [_ in never]: never;
     };
     Functions: {
-      [_ in never]: never;
+      is_admin: {
+        Args: Record<PropertyKey, never>;
+        Returns: boolean;
+      };
     };
     Enums: {
       [_ in never]: never;
