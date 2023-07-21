d939502af53d60d88f1d51e711931fa040fbfeb2
michele <torre.michele07@gmail.com>
2023-07-21 17:55:22 +0200

    Added carousel

---
+++ app/portfolio/[role]/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Carousel from "@/components/carousel";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function PortfolioPage() {
+  const [project, setProject] = useState<string | null>("");
+
+  return (
+    <div className="w-full">
+      <div className="mt-20">
+        <Image
+          className="hidden md:block absolute left-1/4 -top-10 w-[800px] -z-10 select-none"
+          src="/img/blue-blur.svg"
+          alt="blur"
+          width={100}
+          height={100}
+          placeholder="empty"
+          draggable={false}
+        />
+        <h1 className="text-primary font-extrabold text-4xl uppercase text-center">
+          View my past works
+        </h1>
+        <h2 className="text-gray-400 text-center">
+          Get inspired and don't believe just to my words, see what I already
+          did for other clients!
+        </h2>
+      </div>
+      <button className="mt-6" onClick={() => setProject("1")}>
+        <Image
+          src="/img/placeholder.png"
+          width={300}
+          height={300}
+          alt="Placeholder"
+          className="rounded-lg"
+          draggable={false}
+        />
+      </button>
+      {project !== null && <Carousel close={() => setProject(null)} />}
+    </div>
+  );
+}
---
+++ components/carousel.tsx
@@ -0,0 +1,66 @@
+import { useRef } from "react";
+
+export default function Carousel({ close }: { close: () => void }) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  return (
+    <div
+      onClick={(e) => {
+        const item = ref.current;
+        if (!item) return;
+        if (item.contains(e.target as Node)) return;
+
+        e.preventDefault();
+        close();
+      }}
+      className="left-0 top-0 z-20 fixed w-screen h-screen flex flex-col items-center justify-center bg-black/70"
+    >
+      <div ref={ref} className="carousel w-1/2 h-1/2 m-auto">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src="/img/placeholder.png" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src="/img/placeholder.png" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src="/img/placeholder.png" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide4" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src="/img/placeholder.png" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
